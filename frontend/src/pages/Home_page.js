de884e899fb34217372132976ec4ea00065b763a
B6409388 <theerabeem@gmail.com>
2024-11-11 12:04:15 +0700

    โครงหน้า

---
+++ frontend/src/pages/Home_page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+function Homepage(params) {
+  return (
+    <div>
+      <Layout />
+    </div>
+  );
+}
+
+export default Homepage;
